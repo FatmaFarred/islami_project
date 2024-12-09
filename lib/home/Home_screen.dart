794d8a8256d2cad77254c6340f76576f485d5b4f
fatma <fatmafarred267@gmail.com>
2024-12-09 18:54:25 +0300

    Intial project. Added empty project. Added splash screen.

---
+++ lib/home/Home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+ static const String routeName= "Home screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islami_pro/home/Home_screen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomeScreen.routeName: (context)=>HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+
+
+    );
+  }
+}
